7f5123bd711ca1195cb083959f63e1725c04b526
Chay Park <chay.park@mail.utoronto.ca>
2024-01-22 03:49:03 +0000

    Data table was incorrect, updated data to write CSV

--- Toronto_Victims_of_Crimes/scripts/01-download_data.R
+++ Toronto_Victims_of_Crimes/scripts/01-download_data.R
@@ -44,7 +44,7 @@ victims_statistics
 
 # Write CSV file
 write_csv(
-  x = victims_resources,
+  x = victims_statistics,
   file = "Toronto_Victims_of_Crimes/inputs/data/unedited_data.csv"
 )
 head(victims_statistics)
