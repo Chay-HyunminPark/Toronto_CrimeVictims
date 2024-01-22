91d86457a5af72b0246cdb465b2d7deeef234ab1
Chay Park <chay.park@mail.utoronto.ca>
2024-01-22 00:57:56 +0000

    Save in scripts folder

---
+++ Toronto_Victims_of_Crimes/scripts/01-download_data.R
@@ -0,0 +1,36 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Chay Park
+# Date: 20 January 2024
+# Contact: chay.park@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: download the actual data in a reproducible way using opendatatoronto. 
+# Save the data: "inputs/data/unedited_data.csv"
+
+#### Save data ####
+
+# Install the package
+install.packages("opendatatoronto")
+
+# See a list of available packages
+library(opendatatoronto)
+packages <- list_packages(limit = 10)
+packages
+
+# Search packages by title
+victims_packages <- search_packages("victims")
+
+victims_packages
+
+# List resources from package
+victims_resources <- victims_packages %>%
+  list_package_resources()
+
+victims_resources
+
+# Download the resources
+victims_statistics <- victims_resources %>%
+  head(1) %>%
+  get_resource()
+
+victims_statistics
