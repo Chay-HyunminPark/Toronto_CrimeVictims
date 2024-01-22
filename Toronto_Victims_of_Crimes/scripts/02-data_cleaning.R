5f4d74d1a589813f5b9dcd9a33b5cdb22167be08
Chay Park <chay.park@mail.utoronto.ca>
2024-01-22 21:24:20 +0000

    Rename crime_data to unedited_data for the coherency on 02-data_cleaning.R file

--- Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
+++ Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
@@ -4,10 +4,10 @@ library(lubridate)
 library(tidyverse)
 
 # Read the CSV file
-crime_data <- read.csv("Toronto_Victims_of_Crimes/inputs/data/unedited_data.csv")
+unedited_data <- read.csv("Toronto_Victims_of_Crimes/inputs/data/unedited_data.csv")
 
 # Filter out unknown values for SEX and rows with 'other' values for SUBTYPE
-cleaned_data <- crime_data %>%
+cleaned_data <- unedited_data %>%
   filter(SEX %in% c("M", "F") & AGE_GROUP != "Unknown" & SUBTYPE != "Other")
 
 # Write and download cleaned_CSV file
