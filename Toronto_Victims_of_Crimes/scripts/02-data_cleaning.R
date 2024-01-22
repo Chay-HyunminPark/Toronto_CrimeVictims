73d65b5207dd6cf84aedaa4298de38e929d98e57
Chay Park <chay.park@mail.utoronto.ca>
2024-01-22 16:34:21 +0000

    Rename filtered_data to cleaned_data

--- Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
+++ Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
@@ -7,12 +7,12 @@ library(tidyverse)
 crime_data <- read.csv("Toronto_Victims_of_Crimes/inputs/data/unedited_data.csv")
 
 # Filter out unknown values for SEX and rows with 'other' values for SUBTYPE
-filtered_data <- crime_data %>%
+cleaned_data <- crime_data %>%
   filter(SEX %in% c("M", "F") & AGE_GROUP != "Unknown" & SUBTYPE != "Other")
 
-# Write cleaned_CSV file
+# Write and download cleaned_CSV file
 write_csv(
-  x = filtered_data,
+  x = cleaned_data,
   file = "Toronto_Victims_of_Crimes/inputs/data/cleaned_data.csv"
 )
-head(filtered_data)
+head(cleaned_data)
