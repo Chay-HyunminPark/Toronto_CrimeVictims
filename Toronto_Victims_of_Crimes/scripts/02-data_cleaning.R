a2b49ba407ef035db56569a0300a377fb39d478f
Chay Park <chay.park@mail.utoronto.ca>
2024-01-22 16:15:42 +0000

    Upload the cleaned_data

--- Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
+++ Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
@@ -10,5 +10,9 @@ crime_data <- read.csv("Toronto_Victims_of_Crimes/inputs/data/unedited_data.csv"
 filtered_data <- crime_data %>%
   filter(SEX %in% c("M", "F") & AGE_GROUP != "Unknown" & SUBTYPE != "Other")
 
+# Write cleaned_CSV file
+write_csv(
+  x = filtered_data,
+  file = "Toronto_Victims_of_Crimes/inputs/data/cleaned_data.csv"
+)
 head(filtered_data)
-
