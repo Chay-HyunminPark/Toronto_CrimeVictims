72180ee7c931beb14a8fa4dd53237c9871b83fc5
Chay Park <chay.park@mail.utoronto.ca>
2024-01-22 05:04:57 +0000

    Update 02-data_cleaning file

--- Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
+++ Toronto_Victims_of_Crimes/scripts/02-data_cleaning.R
@@ -3,12 +3,12 @@ library(knitr)
 library(lubridate)
 library(tidyverse)
 
-# Check and set the working directory if needed
-# setwd("project/Toronto_Victims_of_Crimes")
-
 # Read the CSV file
 crime_data <- read.csv("Toronto_Victims_of_Crimes/inputs/data/unedited_data.csv")
 
 # Filter out unknown values for SEX and rows with 'other' values for SUBTYPE
 filtered_data <- crime_data %>%
-  filter(SEX %in% c("M", "F") & SUBTYPE %in% c("Robbery", "Sexual violation", "Assault"))
+  filter(SEX %in% c("M", "F") & AGE_GROUP != "Unknown" & SUBTYPE != "Other")
+
+head(filtered_data)
+
